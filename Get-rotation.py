1c9fd871b9c6efeacadfbf2ab9c2e284ff465525
Nehal Nevle <nehalsuperdude@gmail.com>
2020-09-19 17:04:32 +0530

    Added rotation

--- Get-rotation.py
+++ Get-rotation.py
@@ -1,77 +1,82 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-img=cv2.imread('09-16 13:19:06.jpg',0)
+cap = cv2.VideoCapture('latest_1024_0304.mp4')
+
+# take first frame of the video
+ret,frame = cap.read()
+frame = cv2.flip(frame, 0)
+img=cv2.imread('latest_512_0304.jpg',0)
 #current = cv2.imread('Cstar.png')
 blur = cv2.GaussianBlur(img,(35,35),0)
-previousimg = cv2.imread('09-16 13:19:06.jpg')
-img1 = cv2.imread('09-16 13:19:06.jpg',1)
+previousimg = cv2.imread('latest_512_0304.jpg')
+img1 = cv2.imread('latest_512_0304.jpg',1)
 ret,thresh = cv2.threshold(blur,20,255,0)
 thresh1 =cv2.bitwise_not(thresh)
 contours,hierarchy = cv2.findContours(thresh1, 1, 2)
-
+x=3.9
 cnt = contours[0]
 leftmost = tuple(cnt[cnt[:,:,0].argmin()][0])
 rightmost = tuple(cnt[cnt[:,:,0].argmax()][0])
 topmost = tuple(cnt[cnt[:,:,1].argmin()][0])
 bottommost = tuple(cnt[cnt[:,:,1].argmax()][0])
+leftmost=(int(leftmost[0]*(x+0.1)),int(leftmost[1]*x))
+rightmost=(int(rightmost[0]*(x+0.1)),int(rightmost[1]*x))
+bottommost=(int(bottommost[0]*(x+0.1)),int(bottommost[1]*x))
+topmost=(int(topmost[0]*(x+0.1)),int(topmost[1]*x))
 
 
-filename = '09-16 13:19:06.jpg'
-img2 = cv2.imread(filename)
-gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-
-# find Harris corners
-gray = np.float32(gray)
-dst = cv2.cornerHarris(gray,2,3,0.4)
-dst = cv2.dilate(dst,None)
-ret, dst = cv2.threshold(dst,0.01*dst.max(),255,0)
-dst = np.uint8(dst)
-
-# find centroids
-ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
-
-# define the criteria to stop and refine the corners
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
-
-# Now draw them
-res = np.hstack((centroids,corners))
-res = np.int0(res)
-img2[res[:,1],res[:,0]]=[0,0,255]
-img2[res[:,3],res[:,2]] = [0,255,0]
-
-filename = '09-16 13:19:06next.jpg'
-img2 = cv2.imread(filename)
-gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-
-# find Harris corners
-gray = np.float32(gray)
-dst = cv2.cornerHarris(gray,2,3,0.4)
-dst = cv2.dilate(dst,None)
-ret, dst = cv2.threshold(dst,0.01*dst.max(),255,0)
-dst = np.uint8(dst)
-
-# find centroids
-ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
-
-# define the criteria to stop and refine the corners
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
-
-# Now draw them
-res = np.hstack((centroids,corners))
-res = np.int0(res)
-img2[res[:,1],res[:,0]]=[0,0,255]
-img2[res[:,3],res[:,2]] = [0,255,0]
-t=input("input time gap")
-time= t*
 
 print leftmost,' ',rightmost,' ',topmost,' ',bottommost
 img1 = cv2.circle(img1,leftmost, 12, (0,255,255), -1)
 img1 = cv2.circle(img1,rightmost, 12, (0,255,255), -1)
 img1 = cv2.circle(img1,topmost, 12, (0,255,255), -1)
 img1 = cv2.circle(img1,bottommost, 12, (0,255,255), -1)
+
+
+# setup initial location of window
+r,h,c,w = 400,100,300,100  # simply hardcoded the values
+track_window = (c,r,w,h)
+
+# set up the ROI for tracking
+roi = frame[r:r+h, c:c+w]
+hsv_roi =  cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
+cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+# Setup the termination criteria, either 10 iteration or move by atleast 1 pt
+term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+i=1
+s=0
+while(1):
+    ret ,frame = cap.read()
+    frame = cv2.flip(frame, 0)
+
+    if ret == True:
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+
+        # apply meanshift to get the new location
+        ret, track_window = cv2.meanShift(dst, track_window, term_crit)
+
+        # Draw it on image
+        x,y,w,h = track_window
+        if s==0:
+            print x,',',y
+            s+=1
+        img2 = cv2.rectangle(frame, (x,y), (x+w,y+h), 255,2)
+        img2 = cv2.circle(img2,leftmost, 12, (0,255,255), -1)
+        img2 = cv2.circle(img2,rightmost, 12, (0,255,255), -1)
+        img2 = cv2.circle(img2,topmost, 12, (0,255,255), -1)
+        img2 = cv2.circle(img2,bottommost, 12, (0,255,255), -1)
+        cv2.imshow('image',img2)
+        k = cv2.waitKey(4) & 0xff
+        if k == 27:
+            break
+    else:
+        break
+print x,',',y
 
 
 
